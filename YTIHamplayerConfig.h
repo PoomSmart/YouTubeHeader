26bfb364357124ca67e28b08f5f8846a900c4eed
PoomSmart <thatchapon.unp@outlook.com>
2021-08-03 17:05:41 +0700

    Some more

--- YTIHamplayerConfig.h
+++ YTIHamplayerConfig.h
@@ -1,6 +1,9 @@
 #import "YTIHamplayerStreamFilter.h"
+#import "YTIHamplayerABRConfig.h"
 
 @interface YTIHamplayerConfig : NSObject
 @property (nonatomic, assign, readwrite) int renderViewType;
-- (YTIHamplayerStreamFilter *)streamFilter;
+@property (nonatomic, assign, readwrite) BOOL useSbdlRenderView;
+@property (nonatomic, strong, readwrite) YTIHamplayerStreamFilter *streamFilter;
+@property (nonatomic, strong, readwrite) YTIHamplayerABRConfig *videoAbrConfig;
 @end
