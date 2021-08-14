6c6b01d03c412f00400b1347cf5a58fe2f54b577
PoomSmart <thatchapon.unp@outlook.com>
2021-08-14 13:48:58 +0700

    Added more

--- MLInnerTubePlayerConfig.h
+++ MLInnerTubePlayerConfig.h
@@ -1,5 +1,7 @@
 #import "YTIMediaCommonConfig.h"
+#import "YTIHamplayerConfig.h"
 
 @interface MLInnerTubePlayerConfig : NSObject
 - (YTIMediaCommonConfig *)mediaCommonConfig;
+- (YTIHamplayerConfig *)hamplayerConfig;
 @end
--- YTHotConfig.h
+++ YTHotConfig.h
@@ -1,7 +1,9 @@
 #import "YTIHotConfigGroup.h"
+#import "YTIHamplayerHotConfig.h"
 
 @interface YTHotConfig : NSObject
 - (YTIHotConfigGroup *)hotConfigGroup;
 - (YTIIosMediaHotConfig *)mediaHotConfig;
+- (YTIHamplayerHotConfig *)hamplayerHotConfig;
 - (bool)iosReleasePipControllerOnMain;
 @end
