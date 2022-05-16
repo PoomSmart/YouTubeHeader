e9ef2d5a18bdd5840c379c74ddeced4c09e3cce5
PoomSmart <thatchapon.unp@outlook.com>
2022-05-16 14:12:46 +0700

    Use YTCommonColorPalette with notice

--- YTPageStyleController.h
+++ YTPageStyleController.h
@@ -1,6 +1,6 @@
-#import "YTColorPalette.h"
+#import "YTCommonColorPalette.h"
 
 @interface YTPageStyleController : NSObject
-+ (YTColorPalette *)currentColorPalette;
++ (YTCommonColorPalette *)currentColorPalette; // For YouTube older than 17.19.2, import/change type to YTColorPalette
 @property (nonatomic, assign, readwrite) NSInteger appThemeSetting;
 @end
