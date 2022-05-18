e7c3c1d23938092a65ae51752e82b27be9ee9a86
PoomSmart <thatchapon.unp@outlook.com>
2022-05-18 22:47:02 +0700

    Added browse IDs

--- YTIBrowseRequest.h
+++ YTIBrowseRequest.h
@@ -2,4 +2,11 @@
 
 @interface YTIBrowseRequest : NSObject
 + (NSString *)browseIDForExploreTab;
++ (NSString *)browseIDForAccountTab;
++ (NSString *)browseIDForActivityTab;
++ (NSString *)browseIDForHomeTab;
++ (NSString *)browseIDForLibraryTab;
++ (NSString *)browseIDForTrendingTab;
++ (NSString *)browseIDForSubscriptionsTab;
++ (NSString *)browseIDForWhatToWatch;
 @end
