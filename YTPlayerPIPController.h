81125ef7b51adf8f7bb9d266daf5156df7ebdcc1
PoomSmart <thatchapon.unp@outlook.com>
2022-05-20 21:37:43 +0700

    Marked some deprecated

--- YTPlayerPIPController.h
+++ YTPlayerPIPController.h
@@ -1,14 +1,15 @@
+#import "GIMMe.h"
 #import "YTSingleVideoController.h"
 
 @interface YTPlayerPIPController : NSObject
 @property (retain, nonatomic) YTSingleVideoController *activeSingleVideo;
-- (instancetype)initWithPlayerView:(id)playerView delegate:(id)delegate;
+- (instancetype)initWithPlayerView:(id)playerView delegate:(id)delegate; // Deprecated, use initWithDelegate:
 - (instancetype)initWithDelegate:(id)delegate;
-- (GIMMe *)gimme;
+- (GIMMe *)gimme; // Deprecated
 - (BOOL)isPictureInPictureActive;
-- (BOOL)canInvokePictureInPicture;
+- (BOOL)canInvokePictureInPicture; // Deprecated, use canEnablePictureInPicture
 - (BOOL)canEnablePictureInPicture;
-- (void)maybeInvokePictureInPicture;
+- (void)maybeInvokePictureInPicture; // Deprecated, use maybeEnablePictureInPicture
 - (void)maybeEnablePictureInPicture;
 - (void)play;
 - (void)pause;
