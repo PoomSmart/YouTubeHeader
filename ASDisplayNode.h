f807d2eabd21c3ddfd38a2c83588e0f18bd13c86
PoomSmart <thatchapon.unp@outlook.com>
2022-09-10 18:49:04 +0700

    Added new things

--- ASDisplayNode.h
+++ ASDisplayNode.h
@@ -3,6 +3,9 @@
 @interface ASDisplayNode : NSObject
 @property (atomic, copy, readwrite) NSArray *yogaChildren;
 @property (nonatomic, copy, readwrite) NSString *accessibilityIdentifier;
+@property (atomic, weak, readonly) ASDisplayNode *yogaParent;
+@property (atomic, assign, readwrite) CGRect frame;
+@property (atomic, assign, readwrite) CGRect bounds;
 - (UIViewController *)closestViewController;
 - (BOOL)isNodeLoaded;
 @end
