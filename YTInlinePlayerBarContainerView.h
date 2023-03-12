0e41caffb1fc7ba0c1f707b638b2795652e183b5
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-03-12 18:20:19 +0700

    Updated

--- YTInlinePlayerBarContainerView.h
+++ YTInlinePlayerBarContainerView.h
@@ -2,6 +2,7 @@
 #import "YTQTMButton.h"
 
 @interface YTInlinePlayerBarContainerView : YTGlassContainerView
+@property (nonatomic, strong, readwrite) UIView *multiFeedElementView;
 @property (nonatomic, assign, readwrite) BOOL showOnlyFullscreenButton;
 @property (nonatomic, assign, readwrite) int layout;
 @property (nonatomic, weak, readwrite) id delegate;
