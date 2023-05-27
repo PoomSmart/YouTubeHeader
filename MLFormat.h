87dd8699f2c7fd30c9c77cfaa4493dcd9939246b
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-05-27 17:36:50 +0700

    HAMMIMEType

--- MLFormat.h
+++ MLFormat.h
@@ -1,15 +1,17 @@
 #import <CoreGraphics/CoreGraphics.h>
+#import "HAMMIMEType.h"
 #import "YTIFormatStream.h"
 
 @interface MLFormat : NSObject <NSCopying>
-@property (nonatomic, readonly, strong) YTIFormatStream *formatStream;
-@property (nonatomic, readonly, strong) NSURL *URL;
-@property (nonatomic, readonly, assign) int width;
-@property (nonatomic, readonly, assign) int height;
-@property (nonatomic, readonly, assign) int singleDimensionResolution;
-@property (nonatomic, readonly, assign) CGFloat FPS;
-@property (nonatomic, readonly, assign) BOOL isAudio;
-@property (nonatomic, readonly, assign) BOOL isVideo;
-@property (nonatomic, readonly, assign) BOOL isText;
+- (HAMMIMEType *)MIMEType;
+- (YTIFormatStream *)formatStream;
+- (NSURL *)URL;
+- (int)width;
+- (int)height;
+- (int)singleDimensionResolution;
+- (CGFloat)FPS;
+- (BOOL)isAudio;
+- (BOOL)isVideo;
+- (BOOL)isText;
 - (NSInteger)compareByQuality:(MLFormat *)format;
 @end
