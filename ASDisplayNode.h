dffb3fd293351319833a24fc0cd9819be2691c6f
Nguyễn Anh Sáng <sn15062008@gmail.com>
2023-11-13 08:42:25 +0700

    Add new property (#8)

--- ASDisplayNode.h
+++ ASDisplayNode.h
@@ -2,6 +2,7 @@
 #import "ASLayoutElementStyleYoga.h"
 
 @interface ASDisplayNode : NSObject
+@property (atomic, copy, readwrite) UIColor *backgroundColor;
 @property (atomic, copy, readwrite) NSArray *yogaChildren;
 @property (nonatomic, copy, readwrite) NSString *accessibilityIdentifier;
 @property (atomic, weak, readonly) ASDisplayNode *yogaParent;
