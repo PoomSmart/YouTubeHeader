ef0ee32c749c1f8f1613ec8118c8fbea2d77c7b8
NguyenASang <sn15062008@gmail.com>
2023-12-14 14:08:24 +0700

    Merge branch 'main' into addition

--- ASCollectionView.h
+++ ASCollectionView.h
@@ -1,4 +1,6 @@
 #import <UIKit/UIKit.h>
+#import "ASCollectionNode.h"
 
 @interface ASCollectionView : UICollectionView
+@property (nonatomic, weak, readwrite) ASCollectionNode *collectionNode;
 @end
