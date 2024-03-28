4d9b561d124ce299a49c5c10a12dd7f3145ea7f3
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-03-28 18:04:51 +0700

    New things

---
+++ YTIPlayerBarPlayingState.h
@@ -0,0 +1,5 @@
+#import "GPBMessage.h"
+
+@interface YTIPlayerBarPlayingState : GPBMessage
+@property (nonatomic, assign, readwrite) CGFloat totalTimeSec;
+@end
--- YTModularPlayerBarView.h
+++ YTModularPlayerBarView.h
@@ -2,5 +2,6 @@
 
 @interface YTModularPlayerBarView : UIView
 @property (nonatomic, assign, readonly) CGFloat totalTime;
+@property (nonatomic, assign, readonly) CGFloat mediaTime;
 @property (nonatomic, weak, readwrite) YTMainAppVideoPlayerOverlayViewController *accessibilityDelegate;
 @end
