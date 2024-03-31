12b8dd29c1b01d228ebbf7a93d92259ed563f1db
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-03-31 19:24:11 +0700

    New things

--- UIView+YouTube.h
+++ UIView+YouTube.h
@@ -2,4 +2,5 @@
 
 @interface UIView (YouTube)
 - (BOOL)yt_isVisible;
+- (void)yt_setSize:(CGSize)size;
 @end
--- YTQTMButton.h
+++ YTQTMButton.h
@@ -1,19 +1,13 @@
+#import "YTCommonButton.h"
 #import "YTLightweightQTMButton.h"
 
-@interface YTQTMButton : YTLightweightQTMButton
+@interface YTQTMButton : YTLightweightQTMButton <YTCommonButton>
 + (instancetype)barButtonWithImage:(UIImage *)image accessibilityLabel:(NSString *)accessibilityLabel accessibilityIdentifier:(NSString *)accessibilityIdentifier;
 + (instancetype)button;
 + (instancetype)closeButton;
 + (instancetype)iconButton;
 + (instancetype)textButton;
-@property (nonatomic, assign, readwrite) CGFloat buttonImageTitlePadding;
-@property (nonatomic, assign, readwrite) CGFloat minHitTargetSize;
-@property (nonatomic, assign, readwrite) CGFloat verticalContentPadding;
-@property (nonatomic, assign, readwrite) NSInteger buttonLayoutStyle;
-@property (nonatomic, assign, readwrite) BOOL refreshRendererAfterPageStyling;
 @property (nonatomic, assign, readwrite) BOOL sizeWithPaddingAndInsets;
-@property (nonatomic, copy, readwrite) NSString *accessibilityIdentifier;
-@property (nonatomic, copy, readwrite) NSString *accessibilityLabel;
+- (UILabel *)titleLabel;
 - (void)enableNewTouchFeedback;
-- (void)setTitleTypeKind:(NSInteger)titleTypeKind typeVariant:(NSInteger)typeVariant;
 @end
