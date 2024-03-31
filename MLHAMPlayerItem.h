b05a8b434d43099955e208dba869a487a85c6a1a
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-03-31 13:20:25 +0700

    Update MLHAMPlayerItem.h

--- MLHAMPlayerItem.h
+++ MLHAMPlayerItem.h
@@ -7,4 +7,5 @@
 @property (nonatomic, readonly, strong) MLInnerTubePlayerConfig *config;
 @property (nonatomic, strong, readwrite) id <MLVideoFormatConstraint> videoFormatConstraint;
 - (void)ABRPolicy:(MLABRPolicy *)policy selectableFormatsDidChange:(NSArray <MLFormat *> *)formats;
+- (NSArray <MLFormat *> *)selectableVideoFormats;
 @end
