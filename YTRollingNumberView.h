49b9036e7003db091ef42bb2363dbce5e13520e7
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-04-08 09:51:21 +0700

    YTFontAttributes

--- YTRollingNumberView.h
+++ YTRollingNumberView.h
@@ -1,12 +1,14 @@
-#include <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "YTFontAttributes.h"
 
 @interface YTRollingNumberView : UIView
 @property (nonatomic, strong, readwrite) NSMutableArray *digitViews;
+@property (nonatomic, readonly, strong) UIFont *font;
+@property (nonatomic, readonly, strong) UIColor *color;
+@property (nonatomic, readonly, strong) NSString *updatedCount;
+@property (nonatomic, readonly, strong) NSNumber *updatedCountNumber;
+@property (nonatomic, readonly, strong) YTFontAttributes *fontAttributes;
 - (instancetype)initWithDelegate:(id)delegate;
-- (NSString *)updatedCount;
-- (NSNumber *)updatedCountNumber;
-- (UIFont *)font;
-- (UIColor *)color;
 - (void)setUpdatedCount:(NSString *)updatedCount updatedCountNumber:(NSNumber *)updatedCountNumber font:(UIFont *)font color:(UIColor *)color skipAnimation:(BOOL)skipAnimation;
+- (void)setUpdatedCount:(NSString *)updatedCount updatedCountNumber:(NSNumber *)updatedCountNumber font:(UIFont *)font fontAttributes:(YTFontAttributes *)fontAttributes color:(UIColor *)color skipAnimation:(BOOL)skipAnimation; // 19.14.2+
 @end
