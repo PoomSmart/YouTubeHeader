a7b52ab8d627a2b4114cfa48c181c2f53c894795
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-04-28 15:31:08 +0700

    Update MLAVPlayer.h

--- MLAVPlayer.h
+++ MLAVPlayer.h
@@ -3,11 +3,13 @@
 #import "MLPlayerViewProtocol.h"
 #import "MLPlayerStickySettings.h"
 #import "MLStreamSelectorDelegate.h"
+#import "MLVideoFormatConstraint.h"
 
 @interface MLAVPlayer : NSObject <MLStreamSelectorDelegate>
 @property (nonatomic, readwrite, assign) BOOL active;
 @property (nonatomic, readonly, assign) BOOL externalPlaybackActive;
 @property (nonatomic, readwrite, assign) float rate;
+@property (nonatomic, strong, readwrite) id <MLVideoFormatConstraint> videoFormatConstraint;
 @property (nonatomic, readonly, strong) MLVideo *video;
 @property (nonatomic, readonly, strong) MLInnerTubePlayerConfig *config;
 @property (nonatomic, readonly, strong) MLAVAssetPlayer *assetPlayer;
