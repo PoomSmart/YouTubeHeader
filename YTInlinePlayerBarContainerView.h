41644e2b872afeb9aa0ce21893c1d0d7101f02e9
Dan <38832025+dayanch96@users.noreply.github.com>
2024-06-03 12:24:10 +0300

    Update YTInlinePlayerBarContainerView.h

--- YTInlinePlayerBarContainerView.h
+++ YTInlinePlayerBarContainerView.h
@@ -1,12 +1,14 @@
 #import "YTGlassContainerView.h"
 #import "YTInlinePlayerBarView.h"
+#import "YTModularPlayerBarController.h"
 #import "YTLabel.h"
 #import "YTPlayerBarProtocol.h"
 #import "YTQTMButton.h"
 
 @interface YTInlinePlayerBarContainerView : YTGlassContainerView
 @property (nonatomic, strong, readwrite) YTInlinePlayerBarView *playerBar; // Replaced by segmentablePlayerBar in newer versions
-@property (nonatomic, strong, readwrite) id <YTPlayerBarProtocol> segmentablePlayerBar;
+@property (nonatomic, strong, readwrite) id <YTPlayerBarProtocol> segmentablePlayerBar; // Replaced by modularPlayerBar in newer 19.22.3+
+@property (nonatomic, strong, readwrite) YTModularPlayerBarController *modularPlayerBar;
 @property (nonatomic, strong, readwrite) UIView *multiFeedElementView;
 @property (nonatomic, strong, readwrite) YTLabel *durationLabel;
 @property (nonatomic, assign, readwrite) BOOL showOnlyFullscreenButton;
