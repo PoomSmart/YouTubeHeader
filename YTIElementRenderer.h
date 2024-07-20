d2b7e848623b04cdb7b87d43c700b86a1920f600
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-07-20 09:09:55 +0700

    Update YTIElementRenderer.h

--- YTIElementRenderer.h
+++ YTIElementRenderer.h
@@ -4,4 +4,5 @@
 @property (nonatomic, strong, readwrite) YTIElementRendererCompatibilityOptions *compatibilityOptions;
 @property (nonatomic, assign, readwrite) BOOL hasCompatibilityOptions;
 @property (nonatomic, assign, readwrite, setter=yt_setIsRemovedByDismissal:) BOOL yt_isRemovedByDismissal;
+@property (nonatomic, copy, readwrite) NSData *trackingParams;
 @end
