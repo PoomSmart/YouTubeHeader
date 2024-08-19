850e939ce7053f593ed477e83ec914c98669be46
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-08-19 19:29:10 +0700

    Update YTReelModel.h

--- YTReelModel.h
+++ YTReelModel.h
@@ -4,5 +4,6 @@
 @interface YTReelModel : YTReelContentModel
 @property (nonatomic, readonly, assign) int videoType;
 @property (nonatomic, copy, readwrite) NSString *joinVideoID;
+@property (nonatomic, copy, readwrite) NSString *onesieVideoID;
 - (YTIPlayerResponse *)playerResponseOverride;
 @end
