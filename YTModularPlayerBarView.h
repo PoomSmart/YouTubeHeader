d59ebbc98e3bb0316d7b3a98b09b509fdc58ac8e
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-09-17 09:54:37 +0700

    Update YTModularPlayerBarView.h

--- YTModularPlayerBarView.h
+++ YTModularPlayerBarView.h
@@ -1,7 +1,7 @@
 #import "YTAdjustableAccessibilityProtocol.h"
 
 @interface YTModularPlayerBarView : UIView
-@property (nonatomic, assign, readonly) CGFloat totalTime;
+@property (nonatomic, assign, readonly) CGFloat totalTime; // Removed in YouTube 19.37.2
 @property (nonatomic, assign, readonly) CGFloat mediaTime;
 @property (nonatomic, weak, readwrite) id <YTAdjustableAccessibilityProtocol> accessibilityDelegate;
 @end
