8794b4ff298fb9252e8c7b059a023a5fdd7cf5c9
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-02-08 17:57:17 +0700

    Add things

--- UIColor+YouTube.h
+++ UIColor+YouTube.h
@@ -3,5 +3,6 @@
 @interface UIColor (YouTube)
 + (instancetype)gid_colorWithRGBHex:(NSUInteger)hex;
 + (instancetype)LOT_colorWithHexString:(NSString *)hexString;
+- (unsigned int)rgbHex;
 - (NSString *)LOT_hexStringValue;
 @end
--- YTBackgroundabilityPolicy.h
+++ YTBackgroundabilityPolicy.h
@@ -1,5 +1,7 @@
-#import <Foundation/Foundation.h>
+#import <Foundation/NSObject.h>
 
 @interface YTBackgroundabilityPolicy : NSObject
+@property (nonatomic, readonly, assign, getter=isBackgroundableByUserSettings) BOOL backgroundableByUserSettings;
+@property (nonatomic, readonly, assign, getter=isPlayableInPictureInPictureByUserSettings) BOOL playableInPiPByUserSettings;
 - (void)addBackgroundabilityPolicyObserver:(id)observer;
 @end
