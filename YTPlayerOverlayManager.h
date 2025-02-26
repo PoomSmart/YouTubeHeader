c544ed3de93eeedc309d1ba00db0ee78bd5edd94
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-02-26 20:02:33 +0700

    Update YTPlayerOverlayManager.h

--- YTPlayerOverlayManager.h
+++ YTPlayerOverlayManager.h
@@ -1,5 +1,7 @@
+#import "YTVarispeedSwitchController.h"
 #import "YTVideoPlayerOverlayDelegate.h"
 
 @interface YTPlayerOverlayManager : NSObject <YTVideoPlayerOverlayDelegate>
+- (YTVarispeedSwitchController *)varispeedController;
 - (void)didPressToggleFullscreen;
 @end
