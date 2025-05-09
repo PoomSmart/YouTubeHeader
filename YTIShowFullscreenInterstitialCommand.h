0c9cb5fe845401752d27a7b6d029d6ccce503742
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-05-09 09:45:51 +0700

    Add things

---
+++ YTIShowFullscreenInterstitialCommand.h
@@ -0,0 +1,6 @@
+#import "YTIModalClientThrottlingRules.h"
+
+@interface YTIShowFullscreenInterstitialCommand : GPBMessage
+@property (nonatomic, readwrite, assign) BOOL hasModalClientThrottlingRules;
+@property (nonatomic, readwrite, strong) YTIModalClientThrottlingRules *modalClientThrottlingRules;
+@end
