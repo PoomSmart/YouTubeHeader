854a92b535b16d1d966e1c4671a62062850de9f4
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-11-05 21:16:31 +0700

    Add things

--- YTColor.h
+++ YTColor.h
@@ -8,6 +8,7 @@
 + (instancetype)black1;
 + (instancetype)black2;
 + (instancetype)black3;
++ (instancetype)blackPureAlpha0;
 + (instancetype)grey3Alpha70;
 + (instancetype)grey4Alpha60;
 + (instancetype)youTubeRed;
--- YTFrostedGlassView.h
+++ YTFrostedGlassView.h
@@ -5,6 +5,7 @@
 @property (nonatomic, strong, readwrite) UIVisualEffectView *blurEffectView;
 @property (nonatomic, strong, readwrite) UIView *overlayView;
 @property (nonatomic, assign, readwrite) CGFloat cornerRadius;
+- (instancetype)initWithBlurEffectStyle:(NSInteger)style;
 - (instancetype)initWithBlurEffectStyle:(NSInteger)style alpha:(CGFloat)alpha;
 - (instancetype)initWithFallbackEffect;
 - (void)setOverlayColor:(UIColor *)color;
