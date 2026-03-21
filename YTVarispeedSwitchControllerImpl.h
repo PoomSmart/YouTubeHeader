42ee6e807c8292be0c7f2372b115f9166d65c167
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2026-03-21 13:27:47 +0700

    Create YTVarispeedSwitchControllerImpl.h

---
+++ YTVarispeedSwitchControllerImpl.h
@@ -0,0 +1,5 @@
+#import "YTVarispeedSwitchControllerDelegate.h"
+
+@interface YTVarispeedSwitchControllerImpl : NSObject
+@property (nonatomic, weak, readwrite) id <YTVarispeedSwitchControllerDelegate> delegate;
+@end
