666e02d782f91e775f69be81d545f9766718ff4e
PoomSmart <3608783+PoomSmart@users.noreply.github.com>
2026-04-25 10:36:18 +0700

    Add things

---
+++ YTHeaderViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIViewController.h>
+
+@interface YTHeaderViewController : UIViewController
+- (BOOL)isTopLevelPage;
+@end
--- YTRightNavigationButtons.h
+++ YTRightNavigationButtons.h
@@ -3,4 +3,6 @@
 @interface YTRightNavigationButtons : UIView
 @property (nonatomic, readwrite, assign) CGFloat leadingPadding;
 @property (nonatomic, readwrite, assign) CGFloat tailingPadding; // Legitimate typo
+- (id)buttonForType:(NSUInteger)type;
+- (void)setButton:(id)button forType:(NSUInteger)type;
 @end
