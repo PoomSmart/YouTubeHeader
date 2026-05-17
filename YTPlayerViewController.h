a8c8597b7a0da285258e7b360eba516aeec164b1
PoomSmart <3608783+PoomSmart@users.noreply.github.com>
2026-05-17 16:07:54 +0700

    Update YTPlayerViewController.h

--- YTPlayerViewController.h
+++ YTPlayerViewController.h
@@ -21,7 +21,6 @@
 - (CGFloat)currentVideoMediaTime;
 - (CGFloat)currentVideoTotalMediaTime;
 - (int)playerViewLayout;
-- (BOOL)isMDXActive;
 - (void)replay;
 - (void)replayWithSeekSource:(int)seekSource;
 - (void)didPressToggleFullscreen;
@@ -30,5 +29,7 @@
 - (void)seekToTime:(CGFloat)time;
 - (id)activeVideoPlayerOverlay; // YTMainAppVideoPlayerOverlayViewController || YTInlineMutedPlaybackPlayerOverlayViewController
 - (YTPlayerView *)playerView;
+- (BOOL)isMDXActive;
 - (BOOL)isCurrentVideoVertical;
+- (BOOL)isInlinePlaybackActive;
 @end
